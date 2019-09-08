b9a65f66f1b7775507fbbb7785ae44b4edf002e8
Dan McCarthy <dan.willy.mccarthy@gmail.com>
2019-09-08 09:40:48 -0500

    Updated colors of initial main window screen
    Updated the initial background of the main window to match that of the C# version of GameBeak. The C++ version of GameBeak has also been updated to use this style for consistency.
    
    This is simply 5 rows of purple on the top, followed by the rest of the screen being pink.

--- NEST/src/MainWindow/MainWindow.cpp
+++ NEST/src/MainWindow/MainWindow.cpp
@@ -45,8 +45,14 @@ MainWindow::MainWindow(QWidget *parent) :
     hLayout->setContentsMargins(0,0,0,0);
     hLayout->addWidget(canvas);
 
+    // Display purple and pink default background.
     QImage image = QImage(256, 240, QImage::Format_RGB32);
-    image.fill(0xFF8080FF);
+    image.fill(QColor(255, 80, 160, 255));
+    for (int x = 0; x < image.width(); x++) {
+        for (int y = 0; y < 5; y++) {
+            image.setPixel(x, y, 0xFF800080);
+        }
+    }
     canvas->drawImage(image);
 }
 
